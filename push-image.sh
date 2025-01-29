aa5edf77804502d958b3ec80e0481576a46a6a72
xiagn825 <xiagn825@gmail.com>
2025-01-29 21:42:01 +0800

    chore: 修改打包脚本

--- push-image.sh
+++ push-image.sh
@@ -9,4 +9,4 @@ docker tag java-demo 127.0.0.1:5005/java-demo:latest
 
 docker push 127.0.0.1:5005/java-demo:latest
 
-kubectl set image deployment/java-demo java-demo=kind-registry:5000/java-demo:latest
+#kubectl set image deployment/java-demo java-demo=kind-registry:5000/java-demo:latest
