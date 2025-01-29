789e88191904bb136cb01258f3a15b8320e68382
xiagn825 <xiagn825@gmail.com>
2025-01-29 11:13:15 +0800

    chore: 修改fluxcd文件

---
+++ create-fluxcd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -o errexit
+
+flux create source git demo \
+  --url=https://github.com/xiagn825/demo \
+  --branch=master \
+  --interval=1m \
+  --export > ./demo-source.yaml
+
+flux create kustomization demo \
+  --target-namespace=default \
+  --source=demo \
+  --path=./ \
+  --prune=true \
+  --wait=true \
+  --interval=30m \
+  --retry-interval=2m \
+  --health-check-timeout=3m \
+  --export > demo-kustomization.yaml
