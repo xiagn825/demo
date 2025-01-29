9aa794290d5b942e1ae7ec9e31582930813ba561
xiagn825 <xiagn825@gmail.com>
2025-01-29 21:29:15 +0800

    chore: 修改kustomize

--- create-fluxcd.sh
+++ create-fluxcd.sh
@@ -3,14 +3,14 @@ set -o errexit
 
 flux create source git demo \
   --url=https://github.com/xiagn825/demo \
-  --branch=master \
+  --branch=main \
   --interval=1m \
   --export > ./demo-source.yaml
 
 flux create kustomization demo \
   --target-namespace=default \
   --source=demo \
-  --path=./ \
+  --path=./kustomize \
   --prune=true \
   --wait=true \
   --interval=30m \
